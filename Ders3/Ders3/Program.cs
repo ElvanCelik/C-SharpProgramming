40a0d21689c2d5acaf2fd7977db2af66edc0e110
Elvan <90777770+ElvanCelik@users.noreply.github.com>
2023-07-23 17:53:41 +0300

    Var/Const Variables
    Vae ve Const veri tiplerinin ne olduğu örneklerle açıklanmıştır.

---
+++ Ders3/Ders3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ders3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //C# programlama dilinde var değişkeni, bir değişkenin türünü belirtmek yerine, derleyicinin değişkenin türünü otomatik olarak çıkarmasını sağlayan bir anahtar sözcüktür. 
+            var x = 5;
+            var isim = "Elvan";
+            var kesirliSayi = 3.14;
+            var flag = true;
+            var karakter = 'E';
+
+            Console.WriteLine(x +" "+ isim + " " + kesirliSayi + " " + flag + " " + karakter);
+            Console.ReadLine();
+
+            //Const: Constant Kelimesinden gelir. const, sabit değerleri tanımlamak için kullanılan bir anahtar sözcüktür. Bir değişkenin değerinin programın çalışma süresi boyunca değiştirilmemesi gerektiğinde, const anahtar kelimesi kullanılarak bu tür değişkenler tanımlanır.
+            const double PI = 3.14;
+            Console.WriteLine(PI);
+
+        }
+    }
+}
