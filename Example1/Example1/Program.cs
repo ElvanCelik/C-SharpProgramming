5b019c84c34343a942e2456103018eae8aa9986a
Elvan <90777770+ElvanCelik@users.noreply.github.com>
2023-07-23 17:36:26 +0300

    Example(I/O)
    Kullanıcıdan alınan isim, yaş, boy(m) değerlerinin ekrana yazılması (I/O Uygulaması)

---
+++ Example1/Example1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Transactions;
+
+namespace Example1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kullanıcıdan ismini yaşını ve boyunu(metre) cinsinden alıp ekrana yazdıran program;
+            string isim;
+            int yas;
+            float boy;
+
+            Console.WriteLine("Lütfen adınızı giriniz : ");
+            isim = Console.ReadLine();
+            Console.WriteLine("Lütfen yaşınızı giriniz : ");
+            yas = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Lütfen boyunuzu metre cinsinden giriniz : ");
+            boy = (float)Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("İsim : " + isim + "\nYaş : " + yas + "\nBoy : " + boy);
+            Console.ReadLine();
+        }
+    }
+}
