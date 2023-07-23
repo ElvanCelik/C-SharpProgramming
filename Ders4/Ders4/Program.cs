7749424710cee8497959b0cdbfd32a08d15d279c
Elvan <90777770+ElvanCelik@users.noreply.github.com>
2023-07-23 22:41:49 +0300

    Ders4
    Aritmetik işlemler işlenmiştir.

---
+++ Ders4/Ders4/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ders4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 5;
+            int sayi2 = 4;
+            //string isim = "elvan";
+            //Console.WriteLine(isim + sayi1 + sayi2); bu satırın çıktısı : 54elvan olur çünkü console writeline integer değerleri stringe çevirir.
+            Console.WriteLine(sayi1 + sayi2);
+            Console.WriteLine(sayi1 - sayi2);
+            Console.WriteLine(sayi1 * sayi2);
+            Console.WriteLine((double)sayi1 / (double)sayi2);
+            Console.WriteLine(sayi1 % sayi2);
+
+        }
+    }
+}
