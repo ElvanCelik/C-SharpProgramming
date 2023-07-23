1a05d47ad6278485a1bf5282bbff257fb145b6a2
Elvan <90777770+ElvanCelik@users.noreply.github.com>
2023-07-23 23:01:04 +0300

    Assignment Operators
    Atama Operatörleri işlenmiştir.

---
+++ Ders5/Ders5/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ders5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Assignment Operators
+            double sayi = 10;
+            Console.WriteLine(sayi);
+            sayi *= 3;
+            Console.WriteLine(sayi);
+            sayi += 2;
+            Console.WriteLine(sayi);
+            sayi /= 4;
+            Console.WriteLine(sayi);
+            sayi += 2;
+            Console.WriteLine(sayi);
+            
+        }
+    }
+}
