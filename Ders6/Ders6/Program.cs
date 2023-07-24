cf7a000d60cce070955df787d988220a7453bf37
Elvan <90777770+ElvanCelik@users.noreply.github.com>
2023-07-24 19:59:37 +0300

    Increment Decrement
    Artırma operatörleri işlenmiştir. Azaltma operatörleri mantığı da aynıdır.

---
+++ Ders6/Ders6/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ders6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Artırma ve azlatma operatörleri- Increment Decrement
+            int x = 5;
+            Console.WriteLine(x);
+            x = x + 1;
+            Console.WriteLine(x);
+            x += 1;
+            Console.WriteLine(x);
+            x++; //postfix: Sayıyı artırdıktan sonra eşitler.
+            Console.WriteLine(x);
+            ++x;//prefix: Sayıyı önce artırıp sonra eşitler.
+            Console.WriteLine(x);
+            Console.ReadLine();
+
+
+        }
+    }
+}
