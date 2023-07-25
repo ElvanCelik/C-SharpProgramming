9806fd907e312c6335d83697a7d60185a92a3524
Elvan <90777770+ElvanCelik@users.noreply.github.com>
2023-07-25 14:23:58 +0300

    Print numbers with for loop
    Program that prints numbers between 1 and N to the console

---
+++ Example 2/Example 2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Example_2
+{
+    internal class Program
+    {
+        //1 ile N arasındaki sayıları konsola yazdıran program;
+        static void Main(string[] args)
+        {
+            int kacaKadar, aralik;
+            Console.WriteLine("Lütfen kaça kadar yazdırmak istediğinizi giriniz : ");
+            kacaKadar = int.Parse(Console.ReadLine());
+            Console.WriteLine("Lütfen sayıları kaçar kaçar artırmak istediğinizi giriniz : ");
+            aralik = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= kacaKadar;i+= aralik)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
