4ade62a54a78938d10a86cd3f5c2fd37f0317f5d
Elvan <90777770+ElvanCelik@users.noreply.github.com>
2023-07-25 13:27:07 +0300

    Multiplication Table
    Çarpım Tablosu

---
+++ Multiplication Table/Multiplication Table/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Multiplication_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 1;
+            int sayi2 = 1;
+            while (sayi1 <= 10)
+            {
+                while (sayi2 <= 10)
+                {
+                    Console.WriteLine(sayi1 + " x " + sayi2 + " = " + (sayi1 * sayi2));
+                    sayi2++;
+                }
+                sayi1++;
+                sayi2 = 1;
+            }
+            Console.ReadLine();
+        }
+    }
+}
